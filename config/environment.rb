331e85069475a485f399b22404d919a23e20118b
pmartens <pieter@cg.nl>
2013-06-23 14:22:20 +0200

    staging env settings

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ require File.expand_path('../application', __FILE__)
 Stplanner::Application.initialize!
 
 # Exception Notification settings
-ClsShop::Application.config.middleware.use ExceptionNotifier,
+Stplanner::Application.config.middleware.use ExceptionNotifier,
                                            :email_prefix => "[#{:application} Env:#{Rails.env}] ",
                                            :sender_address => %{"#{:application}" <pieter.en.mieke@gmail.com},
                                            :exception_recipients => %w{pieter.en.mieke@gmail.com}
